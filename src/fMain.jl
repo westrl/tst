c523b51107757cf184246d0863150608ab792320
Bob West <westrl@vt.edu>
2020-11-28 17:56:01 -0500

    fMain, model f to fTst module

--- src/fMain.jl
+++ src/fMain.jl
@@ -5,11 +5,6 @@
 using ForwardDiff
 using Test
 
-function f(x::AbstractArray)::Real
-  f = 2.0*x[1]^2 + 3.0*x[2]
-  return f
-end
-
 function main()
   x::AbstractArray = [2.0, 2.0]
   @show f(x)
@@ -19,7 +14,7 @@ function main()
   
   h = ForwardDiff.hessian(f, x)
   @show h
-  
+
 end
 
 main()
